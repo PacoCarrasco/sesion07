cb9530444e7ab28742f3dbaae73aa00257e39323
PacoCarrasco <jose.fco.cd@gmail.com>
2022-08-11 02:39:40 +0000

    se agrega el reto 2

--- Sesión07_2.R
+++ Sesión07_2.R
@@ -24,7 +24,7 @@ library(RMySQL)
 # lectura, los dem?s par?metros son los que nos dan acceso a la BDD.
 
 MyDataBase <- dbConnect(
-  drv = RMySQL::MySQL(),
+ drv = RMySQL::MySQL(),
   dbname = "shinydemo",
   host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
   username = "guest",
@@ -71,6 +71,8 @@ dbDisconnect(MyDataBase)
 
 ################################################################################
 ## RETO 01: RSTUDIO CLOUD -> GITHUB
+
+
 
 ################################################################################
 ## EJEMPLO 3: VARIANTES EN LA LECTURA DE BDD CON R
@@ -236,6 +238,39 @@ str(table)
 
 ################################################################################
 ## RETO 2: EXTRACCI?N DE TABLAS EN UN HTML
+library(rvest)
 
+theurl <- "https://www.glassdoor.com.mx/Sueldos/data-scientist-sueldo-SRCH_KO0,14.htm"
+
+file<-read_html(theurl)
+
+tables<-html_nodes(file, "table")
+# Hay que analizar 'tables' para determinar cual es la posición en la lista que contiene la tabla, en este caso es la no. 4 
+
+table1 <- html_table(tables[1], fill = TRUE)
+
+
+table <- na.omit(as.data.frame(table1))
+
+str(table)
+
+#Removiendo caracteres inncesarios 
+a <- gsub("MXN","",table$Sueldo)
+a <- gsub("[^[:alnum:][:blank:]?]", "", a)
+a <- gsub("mes", "", a)
+a <- as.numeric(a)
+table$Sueldo <- a
+
+#Removiendo caracteres inncesarios
+b <- gsub("Sueldos para Data Scientist en ", "", table$Cargo)
+table$Cargo <-b
+
+#Máximo sueldo
+max.sueldo <- which.max(table$Sueldo)
+table[max.sueldo,]
+
+#Mínimo sueldo
+min.sueldo <- which.min(table$Sueldo)
+table[min.sueldo,]
 ################################################################################
 ## POSTWORK
