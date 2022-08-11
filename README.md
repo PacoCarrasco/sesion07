a0af0480bf057b3c6ab7712e706adff59714b65f
PacoCarrasco <jose.fco.cd@gmail.com>
2022-08-11 01:12:19 +0000

    Archivos nuevo proyecto de R

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bienvebidos a mi repositorio
+
+Este es un repositorio de mi clase de bedu
+
--- script_prueba.R
+++ script_prueba.R
@@ -3,5 +3,8 @@
 iris_db<-iris
 head(iris_db)
 
+summary(iris_db)
 
+##token
+##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
 
